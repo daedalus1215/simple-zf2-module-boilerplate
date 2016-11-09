fa36aee177e94b1269cea0b4a91445dbf79930e3
larry adams <ladams1776@yahoo.com>
2016-11-09 08:37:53 -0500

    fixed the layout stuff, that if this was being plugged into a legacy/zf2 application that it will work - before only worked if it was a legit zf2 application with a proper layout setup

--- Login/config/module.config.php
+++ Login/config/module.config.php
@@ -17,8 +17,7 @@ return [
                     // Change this to something specific to your module
                     'route'    => '/login',
                     'defaults' => [
-                        '__NAMESPACE__' => 'Login\Controller',
-                        'controller'    => 'Index',
+                        'controller' => 'Login\Controller\Index',
                         'action'        => 'index',
                     ],
                 ],
@@ -31,6 +30,16 @@ return [
         ],
     ],
     'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
+        ),
         'template_path_stack' => [
             'login' => __DIR__ . '/../view',
         ],
